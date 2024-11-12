ac9a2f9aa9fe37c7608971e4006075e83bd1e888
Jon <jonfernandez.sanluis@gmail.com>
2024-11-12 13:10:48 +0100

    martes

--- js/script.js
+++ js/script.js
@@ -4,16 +4,26 @@ let password = "123";
 
 // Funcion que valida el inicio de sesión
 function validarInicioSesion() {
+
+    // evita que el formulario se envie en caso de que sea erroneo
+   // event.preventDefault();
+
     // recoge los datos que ha introducido el usuario en el formulario
-    let inputName = document.getElementById("usuario").value;
-    let inputPassword = document.getElementById("password").value;
+    let inputName = document.getElementById("usuario");
+    let inputPassword = document.getElementById("password");
 
     // bloque condicional que compara los datos del formulario con las credenciales
-    if (inputName == name && inputPassword == password) {
+    if (inputName.value == name && inputPassword.value == password) {
         console.log("Valido");
     } else {
         console.log("No valido");
+        // recoger elementos del html
         let textoError = document.getElementById("pErrorInicioSesion");
-        textoError.innerHTML = "Error";
+        textoError.innerHTML = "Nombre de usuario o contraseña incorrectos"; // mensaje de error en pantalla para notificar al usuario
+
+        // vaciar inputs
+        inputName.value = "";
+        inputPassword.value = "";
     }
 }
+//document.getElementById("formulario").addEventListener("submit", validarInicioSesion);
