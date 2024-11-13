0fa35c23c8b70183b33326282cb6151de63a6598
Jon <jonfernandez.sanluis@gmail.com>
2024-11-13 09:31:46 +0100

    nav mas centrado

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 // Credenciales
-let name = "dcd";
-let password = "123";
+let usuarios = [{name: "jon", password: "123"},
+                {name: "ibai", password: "123"},
+                {name: "beverly", password: "123"},
+                {name: "markel", password: "123"},
+                {name: "daniela", password: "123"}
+                ];
 
 // Funcion que valida el inicio de sesión
 function validarInicioSesion() {
@@ -9,19 +13,24 @@ function validarInicioSesion() {
     let inputName = document.getElementById("usuario");
     let inputPassword = document.getElementById("password");
 
-    // bloque condicional que compara los datos del formulario con las credenciales
-    if (inputName.value == name && inputPassword.value == password) {
-        console.log("Valido");
-    } else {
-        console.log("No valido");
-        // recoger elementos del html
-        let textoError = document.getElementById("pErrorInicioSesion");
-        textoError.innerHTML = "Nombre de usuario o contraseña incorrectos"; // mensaje de error en pantalla para notificar al usuario
+    // bucle para recorrer array de usuarios
+    for (let i = 0; i < usuarios.length; i++) {
+        // bloque condicional que compara los datos del formulario con las credenciales
+        if (inputName.value == usuarios[i].name && inputPassword.value == usuarios[i].password) {
+            console.log("Valido");
+            break;
+        } else {
+            console.log("No valido");
+            // recoger elementos del html
+            let textoError = document.getElementById("pErrorInicioSesion");
+            textoError.innerHTML = "Nombre de usuario o contraseña incorrectos"; // mensaje de error en pantalla para notificar al usuario
 
-        // vaciar inputs
-        inputName.value = "";
-        inputPassword.value = "";
+            // vaciar inputs
+            inputName.value = "";
+            inputPassword.value = "";
+        }
     }
+    
 }
 
 /*
