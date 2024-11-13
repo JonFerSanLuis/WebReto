aa5cc5687b6f675614f063b94f29285d01d7cb74
Jon <jonfernandez.sanluis@gmail.com>
2024-11-13 12:27:23 +0100

    iniciar sesion eliminado

--- js/script.js
+++ js/script.js
@@ -6,19 +6,24 @@ let usuarios = [{name: "jon", password: "123"},
                 {name: "daniela", password: "123"}
                 ];
 
-// Funcion que valida el inicio de sesión
-function validarInicioSesion() {
 
+// Funcion que valida el inicio de sesión
+function validarInicioSesion(evento) {
+
+    evento.preventDefault();
     // recoge los datos que ha introducido el usuario en el formulario
-    let inputName = document.getElementById("usuario");
-    let inputPassword = document.getElementById("password");
+    const inputName = document.getElementById("usuario");
+    const inputPassword = document.getElementById("password");
 
     // bucle para recorrer array de usuarios
     for (let i = 0; i < usuarios.length; i++) {
         // bloque condicional que compara los datos del formulario con las credenciales
         if (inputName.value == usuarios[i].name && inputPassword.value == usuarios[i].password) {
-            console.log("Valido");
+
+            // Si las credenciales son correctas, guardamos un token en localStorage
+            localStorage.setItem("isLoggedIn", "true");
             break;
+
         } else {
             console.log("No valido");
             // recoger elementos del html
@@ -33,8 +38,6 @@ function validarInicioSesion() {
     
 }
 
-/*
-function redirigirIniciarSesion() {
-    window.location.href = "../html/inciarSesion.html";
+function verificarSesion() {
+    
 }
-*/
